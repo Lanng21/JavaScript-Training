e6fdfda3f58b3d75f9822320c0cc6dfa0d4c3d76
Milo <mailanbiun@gmail.com>
2022-10-30 18:24:28 +0700

    feat/practice-function-js

---
+++ function/test.js
@@ -0,0 +1,17 @@
+// const power = (base, exponent) => {
+//     let result = 1;
+//     for (let count = 0; count < exponent; count++) {
+//     result *= base;
+//     }
+//     return result;
+// };
+//     console.log(power(3,5))
+
+
+const square1 = (x) => { return x * x; };
+const square2 = x => x * x;
+
+const horn = () => {
+    console.log("Toot");
+    };
+    
